fe0db38f88aa2267f051ba00688815f39a0f762c
= <=>
2021-05-15 00:27:15 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="filemanager/extend/js/jquery-3.6.0.min.js"></script>
+    <script src="filemanager/extend/js/jquery-ui.js"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <script src="filemanager/extend/js/filemanager.js"></script>
+    <script>
+        const filemanager = new Filemanager();
+    </script>
+</body>
+</html>
